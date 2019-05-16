0ea4a0fd93246f48f0bb293a2922d7cc60bb3d6d
olenick-eyoon <eyoon@olenick.com>
2019-05-16 17:35:06 -0300

    with spreads and reduce
    Couldn't figure out where to apply map

---
+++ cartesianProduct.js
@@ -0,0 +1,53 @@
+function addToLog(logTitle, logText) {
+  $("body").append("<p><b>" + logTitle + "</b><br/>" + logText + "</p>");
+}
+
+function render(product) {
+  if (Array.isArray(product)) {
+    return product.reduce(function(text, pair) {
+      return text + "{ " + pair.join(" , ") + " }" + "<br/>";
+    }, "");
+  }
+}
+
+function cartesianProduct(product, pair, sets) {
+  const [first, ...rest] = sets;
+
+  if (!Array.isArray(first)) {
+    return [...product, pair];
+  }
+
+  return first.reduce(function(subProduct, element) {
+    return cartesianProduct(subProduct, [...pair, element], rest);
+  }, product);
+}
+
+function getCartesianProduct() {
+  let sets = [];
+
+  if (arguments.length < 2) {
+    return "At least two sets are required";
+  }
+  console.log(arguments)
+  for (var setIndex = 0; setIndex < arguments.length; setIndex++) {
+    if (!Array.isArray(arguments[setIndex])) {
+      return "At least one parameter is not an array";
+    }
+
+    if (arguments[setIndex].length === 0) {
+      return "Empty Cartesian Product";
+    }
+
+    sets.push(arguments[setIndex]);
+  }
+
+  return cartesianProduct([], [], sets);
+}
+
+let set1 = [9, 3, 1, 7, 8];
+let set2 = [6, 2, 4, 0, 5];
+let set3 = [1, 23];
+
+let cp = getCartesianProduct(set1, set2, set3);
+
+addToLog("Cartesian Product", render(cp));
